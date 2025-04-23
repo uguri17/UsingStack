76ca09184b2765a28a79604589884ec7ee5524a3
RND-HW3 <RND-HW3@RND-HW3>
2025-04-24 08:20:19 +0900

    초기 커밋.

---
+++ UsingStack/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace UsingStack
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }            
+        }
+    }
+}
